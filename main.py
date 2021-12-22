697c59e78d224e3d41704ed16627f23afb39ba9b
yymin1022 <yymin1022@gmail.com>
2021-12-22 19:12:14 +0900

    Update main.py : Add API Functions

--- main.py
+++ main.py
@@ -5,6 +5,18 @@ flaskApp = Flask(__name__)
 @flaskApp.route("/")
 def main():
     return "Hello, CECOM! API Main Function!"
+ 
+@flaskApp.route("/getPostList")
+def main():
+    return "CECOM Web API : Get Post List Function"
+ 
+@flaskApp.route("/getPost")
+def main():
+    return "CECOM Web API : Get Post Function"
+ 
+@flaskApp.route("/writePost")
+def main():
+    return "CECOM Web API : Write Post Function"
 
 if __name__ == "__main__":
     flaskApp.run(host="0.0.0.0", port=80)
